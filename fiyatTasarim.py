57f49cba5f7be2da5500ae893c64da04dc384ce4
kymkypn <eksot1@hotmail.com>
2020-09-01 16:37:12 +0300

    Update fiyatTasarim.py

--- fiyatTasarim.py
+++ fiyatTasarim.py
@@ -1,8 +1,5 @@
-from tkinter import *
-from parsel import Selector
-from openpyxl import load_workbook
-import re , requests ,time
 import tkinter as tk
+from tkinter import *
 
 
 def trendyol():
@@ -23,6 +20,7 @@ def n11():
 
 def temizle():
     print("Temizle Geldi")
+    sagBolge.delete("1.0", tk.END)
 
 
 def cik():
@@ -31,30 +29,33 @@ def cik():
 
 
 anaPencere=tk.Tk()
-anaPencere.geometry("600x300")
+anaPencere.geometry("760x440")
 anaPencere.title("Pazar Yeri Fiyatları")
 anaPencere.iconbitmap(r'C:\Users\pc\Desktop\den\venv\icon.ico')
+
+sagBolge=Text(anaPencere)
+sagBolge.grid(column=0,row=0,padx=5,pady=5)
 
 solDugmeler=Frame(anaPencere)
 solDugmeler.grid()
 
 trenyolDugme=Button(solDugmeler,command=trendyol,text="Trendyol",height=1,width=15)
-trenyolDugme.grid(padx=10,pady=10)
+trenyolDugme.grid(column=1,row=1,padx=5,pady=5)
 
 hepsiBuradaDugme=Button(solDugmeler,command=hepsiBurada,text="Hepsi Burada",height=1,width=15)
-hepsiBuradaDugme.grid(padx=10,pady=10)
+hepsiBuradaDugme.grid(column=2,row=1,padx=5,pady=5)
 
 n11Dugme=Button(solDugmeler,command=n11,text="N11",height=1,width=15)
-n11Dugme.grid(padx=10,pady=10)
+n11Dugme.grid(column=3,row=1,padx=5,pady=5)
 
 gittiGidiyorDugme=Button(solDugmeler,command=gittiGidiyor,text="Gitti Gidiyor",height=1,width=15)
-gittiGidiyorDugme.grid(padx=10,pady=10)
+gittiGidiyorDugme.grid(column=4,row=1,padx=5,pady=5)
 
 temizleDugme=Button(solDugmeler,command=temizle,text="Listeyi Temizle",height=1,width=15)
-temizleDugme.grid(padx=10,pady=10)
+temizleDugme.grid(column=5,row=1,padx=5,pady=5)
 
 cikDugme=Button(solDugmeler,command=cik,text="Çıkış Yap",height=1,width=15)
-cikDugme.grid(padx=10,pady=10)
+cikDugme.grid(column=6,row=1,padx=5,pady=5)
 
 
 
